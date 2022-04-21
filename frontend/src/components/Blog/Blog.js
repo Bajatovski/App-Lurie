6d0828724d5c2cec752c41eb4124a0db2050886a
Bajatovski <janaki.bajatovski@gmail.com>
2022-04-21 15:57:22 +0200

    Blog added, finished

--- frontend/src/components/Blog/Blog.js
+++ frontend/src/components/Blog/Blog.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import picture from '../../images/download.jpg'
 
 
 function Blog() {
+    const [showMoreOne, setShowMoreOne] = useState(false);
+    const [showMoreTwo, setShowMoreTwo] = useState(false);
+    const [showMoreThree, setShowMoreThree] = useState(false);
+    const [showMoreFour, setShowMoreFour] = useState(false);
+
+    const  textOne = "tetexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttexttexttexttexttexttextttextxt";
+    const  textTwo = "tetexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttexttexttexttexttexttextttextxt";
+    const  textThree = "tetexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttexttexttexttexttexttextttextxt";
+    const  textFour = "tetexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttexttexttexttexttexttextttextxt";
+    {/* Write the content in another text const as shown above, you have to make another const showMoreFive for example
+                    and make the same logic  */}
+    
     return(
         <div className="container my-5">
             <div className="row mt-5">
@@ -17,8 +29,15 @@ function Blog() {
                 <img src={picture} class="card-img-top" alt="..." style={{width: "auto", height: "350px"}}></img>
                     <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read full text</a>
+                    <p>
+                        <p>
+                         {showMoreOne ? textOne : `${textOne.substring(0, 130)}`}
+                         </p>
+                         <button className="btn btn-primary" onClick={() => setShowMoreOne(!showMoreOne)}>
+                         {showMoreOne ? "  Show less" : "Show more"}
+                         </button>
+
+                    </p>
                     </div>
                 </div>
                 </div>
@@ -31,13 +50,69 @@ function Blog() {
                 <img src={picture} alt="..." style={{width: "auto", height: "350px"}}></img>
                     <div class="card-body">
                     {/* Write the title here */} <h5 class="card-title">Card title</h5>
-                    {/* Write the content here */} <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read full text</a>
+                    {/* Write the content here */} <p>
+                        <p>
+                         {showMoreTwo ? textTwo : `${textTwo.substring(0, 150)}`}
+                         </p>
+                         <button className="btn btn-primary" onClick={() => setShowMoreTwo(!showMoreTwo)}>
+                         {showMoreTwo ? "  Show less" : "Show more"}
+                         </button>
+
+                    </p>
                     </div>
                 </div>
                 </div>
                 <div className="col-sm-2"></div>
                 </div>
+                <div className="container my-5">
+            <div className="row mt-5">
+                <div className="col-sm-2"></div>
+                    <div className="col-sm-12 col-lg-4 my-5">
+                    <div class="card ">
+                        
+            {/*Whatever image you want, just download it in the PICTURES folder of this project,
+                and write: import imagee from '../pictures/test.jpg' or whatever name is the picture
+                and put it in between the curly braces of the src tag  */} 
+                <img src={picture} class="card-img-top" alt="..." style={{width: "auto", height: "350px"}}></img>
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p>
+                        <p>
+                         {showMoreThree ? textOne : `${textThree.substring(0, 130)}`}
+                         </p>
+                         <button className="btn btn-primary" onClick={() => setShowMoreThree(!showMoreThree)}>
+                         {showMoreThree ? "  Show less" : "Show more"}
+                         </button>
+
+                    </p>
+                    </div>
+                </div>
+                </div>
+                <div className="col-sm-12 col-lg-4 my-5">
+                    <div class="card">
+                        
+                {/*Whatever image you want, just download it in the PICTURES folder of this project,
+                and write: import imagee from '../pictures/test.jpg' or whatever name is the picture
+                and put it in between the curly braces of the src tag  */} 
+                <img src={picture} alt="..." style={{width: "auto", height: "350px"}}></img>
+                    <div class="card-body">
+                    {/* Write the title here */} <h5 class="card-title">Card title</h5>
+                    {/* Write the content in another text const as shown above, you have to make another const showMoreFive for example
+                    and make the same logic  */} <p>
+                        <p>
+                         {showMoreFour ? textTwo : `${textFour.substring(0, 150)}`}
+                         </p>
+                         <button className="btn btn-primary" onClick={() => setShowMoreFour(!showMoreFour)}>
+                         {showMoreFour ? "  Show less" : "Show more"}
+                         </button>
+
+                    </p>
+                    </div>
+                </div>
+                </div>
+                <div className="col-sm-2"></div>
+                </div>
+      </div>
       </div>
       
     )
